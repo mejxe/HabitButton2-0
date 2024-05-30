6607a4338370f33794947f80ad2c0ccaed8cb7f7
mejxe <mlaskowski707@gmail.com>
2024-05-30 11:28:08 +0200

    cleanup

--- graph_create.py
+++ graph_create.py
@@ -33,7 +33,5 @@ def update(graph_endpoint):
     graph = requests.put(graph_endpoint, headers=headers, json=graph_data)
     print(graph.text)
 
-def update_user(user):
-    print(requests.put(f"https://pixe.la/v1/users/{user}", headers=headers, json={"thanksCode":"22c5698062c595820b6b815ae2cd76b83da9eda658402bce3bc0a066c0ddb250"}).text)
 
 create()
